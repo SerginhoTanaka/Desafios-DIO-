003edcace4edcff4bde95e8467160b6e857d7bc0
Sergio Kenji Sawasaki Tanaka <serginho.tanaka@edu.unifil.br>
2022-11-01 14:50:33 -0300

    nomeIdade

---
+++ exercicios/src/nomeIdade.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class nomeIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+        int idade;
+        System.out.println("para finalizar o Loop,digite 0" );
+        while (true) {
+            System.out.println("Nome:");
+            nome = scanner.next();
+            if (nome.equals("0")) break;
+            System.out.println("Idade:");
+            idade = scanner.nextInt();
+
+
+        }
+    }
+
+}
